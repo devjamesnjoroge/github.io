fc8f9789e8c14c67053e3baf3086e0c796a0f7f7
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-02-26 21:28:47 +0300

    it will add toggle menu dropdown

--- js/script.js
+++ js/script.js
@@ -6,6 +6,28 @@ var one = document.querySelector("#one");
 var two = document.querySelector("#two");
 var three = document.querySelector("#three");
 var four = document.querySelector("#four");
+var hambg = document.querySelector("#hambg");
+var nav = document.querySelector(".nav-col");
+var cross = document.querySelector("#cross");
+var aselect = document.querySelector(".hidev");
+
+hambg.addEventListener("click", function (event) {
+  nav.style.display = "flex";
+  hambg.style.display = "none";
+  cross.style.display = "block";
+});
+
+cross.addEventListener("click", function (event) {
+  nav.style.display = "none";
+  hambg.style.display = "block";
+  cross.style.display = "none";
+});
+
+aselect.addEventListener("click", function (event) {
+  nav.style.display = "none";
+  hambg.style.display = "block";
+  cross.style.display = "none";
+});
 
 pg1.addEventListener("click", function (event) {
   if ((one.style.display = "none")) {
--- test.js
+++
@@ -1,7 +0,0 @@
-var x = 0;
-
-for (var i = 0; i < 4; i++) {
-  for (var j = 0; j < 5; j++) {
-    console.log(x++);
-  }
-}
